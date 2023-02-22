7fb3dbd9b875116da5e4bc1863a771eedf4b8b77
Antonio Pedro <tonio.pedro99@gmail.com>
2023-02-22 16:57:11 +0530

    Add fragment

--- a2_alarm_app/app/src/main/java/com/antonio20028/alarmapp/MainActivity.kt
+++ a2_alarm_app/app/src/main/java/com/antonio20028/alarmapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.antonio20028.alarmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.antonio20028.alarmapp.ui.AlarmFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val fragManager: FragmentManager = supportFragmentManager
+        val fragTransaction: FragmentTransaction = fragManager.beginTransaction()
+
+        val alarmFragment = AlarmFragment()
+        fragTransaction.replace(R.id.container_view, alarmFragment).commit()
+
     }
+
+
 }
