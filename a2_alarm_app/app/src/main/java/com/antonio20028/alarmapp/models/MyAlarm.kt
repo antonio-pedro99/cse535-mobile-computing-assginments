cfe1c4aa90c34f2287c31bf05760605a9fea3b1c
Antonio Pedro <tonio.pedro99@gmail.com>
2023-02-22 22:35:23 +0530

    improve alarm fragment

--- a2_alarm_app/app/src/main/java/com/antonio20028/alarmapp/models/MyAlarm.kt
+++ a2_alarm_app/app/src/main/java/com/antonio20028/alarmapp/models/MyAlarm.kt
@@ -2,6 +2,8 @@ package com.antonio20028.alarmapp.models
 
 data class Alarm(
     val name: String,
+    val selectedHour:Int = 0,
+    val selectedMinute:Int = 0,
     val inputTime:String, //e.g 00:00
     val format: String, //AM, PM
 ) {
