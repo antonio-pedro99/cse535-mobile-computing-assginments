627aeb0960dc66fc1bc871a4d47d4b2a2d132168
Antonio Pedro <tonio.pedro99@gmail.com>
2023-02-24 20:41:16 +0530

    Assignment done

--- a2_alarm_app/app/src/main/java/com/antonio20028/alarmapp/broadcasts/CustomIntentFilter.kt
+++ a2_alarm_app/app/src/main/java/com/antonio20028/alarmapp/broadcasts/CustomIntentFilter.kt
@@ -1,7 +1,5 @@
 package com.antonio20028.alarmapp.broadcasts
 
-import android.content.IntentFilter
-
 object CustomIntentFilter {
     const val ACTION_PHONE_CALL_DETECTED =  "com.antonio20028.alarmapp.PHONE_CALL"
     const val ACTION_POWER_CONNECTED_DETECTED = "com.antonio20028.alarmapp.POWER_CONNECTED"
