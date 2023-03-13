b905b549a187283e0312c68d6d7dbd7251e0c4b2
Antonio Pedro <tonio.pedro99@gmail.com>
2023-03-13 17:14:03 +0530

    First commit

--- a2_alarm_app/app/src/main/java/com/antonio20028/alarmapp/broadcasts/CustomIntentFilter.kt
+++
@@ -1,7 +0,0 @@
-package com.antonio20028.alarmapp.broadcasts
-
-object CustomIntentFilter {
-    const val ACTION_PHONE_CALL_DETECTED =  "com.antonio20028.alarmapp.PHONE_CALL"
-    const val ACTION_POWER_CONNECTED_DETECTED = "com.antonio20028.alarmapp.POWER_CONNECTED"
-    const val ACTION_USER_STOP_DETECTED = "com.antonio20028.alarmapp.USER_STOP"
-}
