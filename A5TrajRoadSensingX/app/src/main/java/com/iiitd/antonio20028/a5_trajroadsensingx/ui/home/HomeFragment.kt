7796b22474f698bb7a56380ae3e5e14bb8e28e19
Antonio Pedro <tonio.pedro99@gmail.com>
2023-04-21 04:11:33 +0530

    a5 - show user direction

--- A5TrajRoadSensingX/app/src/main/java/com/iiitd/antonio20028/a5_trajroadsensingx/ui/home/HomeFragment.kt
+++ A5TrajRoadSensingX/app/src/main/java/com/iiitd/antonio20028/a5_trajroadsensingx/ui/home/HomeFragment.kt
@@ -1,15 +1,24 @@
 package com.iiitd.antonio20028.a5_trajroadsensingx.ui.home
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.iiitd.antonio20028.a5_trajroadsensingx.R
 import com.iiitd.antonio20028.a5_trajroadsensingx.utils.changeStatusBarColor
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
+    private lateinit var homeFragmentViewModel: HomeFragmentViewModel
+    private lateinit var directionSensorListener: DirectionSensorListener
+    private lateinit var sensorManager: SensorManager
+    private lateinit var rotationVectorSensor: Sensor
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,8 +29,36 @@ class HomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-       activity?.changeStatusBarColor(statusBarColor = activity?.resources?.getColor(R.color.green), navigationBarColor = activity?.resources?.getColor(R.color.white
-       ), true)
+        activity?.changeStatusBarColor(
+            statusBarColor = activity?.resources?.getColor(R.color.green),
+            navigationBarColor = activity?.resources?.getColor(
+                R.color.white
+            ),
+            true
+        )
+        val directionImageView = view.findViewById<ImageView>(R.id.img_compass)
+
+        //get geomagnetic or rotation vector
+        sensorManager =  requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR)
+            ?: sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+
+        homeFragmentViewModel = ViewModelProvider(this).get(HomeFragmentViewModel::class.java)
+        directionSensorListener = DirectionSensorListener(homeFragmentViewModel)
+
+        homeFragmentViewModel.getUserDirection().observe(viewLifecycleOwner){
+            directionImageView.animate().rotation(it).start()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(directionSensorListener, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(directionSensorListener)
     }
 
 
--- A5TrajRoadSensingX/app/src/main/java/com/iiitd/antonio20028/a5_trajroadsensingx/ui/home/HomeFragmentViewModel.kt
+++ A5TrajRoadSensingX/app/src/main/java/com/iiitd/antonio20028/a5_trajroadsensingx/ui/home/HomeFragmentViewModel.kt
@@ -1,6 +1,16 @@
 package com.iiitd.antonio20028.a5_trajroadsensingx.ui.home
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeFragmentViewModel:ViewModel() {
+    private val userDirection = MutableLiveData<Float>()
+
+    fun trackUserDirection(direction:Float){
+        userDirection.apply {
+            value = direction
+        }
+    }
+
+    fun getUserDirection():MutableLiveData<Float> = userDirection
 }
